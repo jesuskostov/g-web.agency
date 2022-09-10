eaf18a10947fb8cb0d0cfa9ff3be34795fb9cbf7
jesuskostov <jesuskostov@gmail.com>
2022-09-10 20:09:20 +0000

    123

--- create.g-web.agency/routes/api.php
+++ create.g-web.agency/routes/api.php
@@ -37,7 +37,5 @@ Route::post('/create/{project}', function ($project) {
     // Create virtualhost
     $ssh->exec('cd /var/www/html/ && virtualhost create ' . $project . '.g-web.agency /var/www/html/' . $project . '.g-web.agency');
     // certbot --apache -d example.domain.com and choose 2
-    $ssh->exec('cd /var/www/html/ && certbot --apache -d --redirect ' . $project . '.g-web.agency');
-    sleep(10);
-    $ssh->exec('2');
+    $ssh->exec('cd /var/www/html/ && certbot --apache -d ' . $project . '.g-web.agency --redirect');
 });
