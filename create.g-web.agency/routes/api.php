e547a14a62e9b8fbb94575e9a5b19c6582a6839c
jesuskostov <jesuskostov@gmail.com>
2022-09-10 18:34:17 +0000

    123

--- create.g-web.agency/routes/api.php
+++ create.g-web.agency/routes/api.php
@@ -15,5 +15,20 @@ use Illuminate\Support\Facades\Route;
 
 // Get all users
 Route::post('/create/{project}', function ($project) {
-    return $project;
+
+    // Global path
+    // $path = '/var/www/html/' . $project . '.g-web.agency';
+
+    // $test = shell_exec('cd /var/www/html && git add . && git commit -m "Create ' . $project . '" && git push origin');
+    $test = shell_exec('cd /var/www/html && git status');
+    echo $test;
+
+    // Check if exist
+    // if (!file_exists($path)) {
+    //     // Create folder
+    //     mkdir($path, 0777, true);
+    //     // Add, commit and push to git
+    // } else {
+    //     echo 'Project already exist';
+    // }
 });
